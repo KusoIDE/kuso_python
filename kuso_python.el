b4df6246f619e6a054c335b4033e52be19d13f75
Sameer Rahmani <lxsameer@gnu.org>
2013-07-17 03:31:21 +0430

    flymake support added

--- kuso_python.el
+++ kuso_python.el
@@ -10,3 +10,21 @@
 
 ;; Setup hideshow
 (add-hook 'python-mode-hook         'hs-minor-mode)
+
+
+(autoload 'python-pylint "python-pylint")
+(autoload 'pylint "python-pylint")
+
+
+(setq KUSO_PYTHON_PATH default-directory)
+(when (load "flymake" t)
+  (defun flymake-pyflakes-init ()
+    (let* (
+           (temp-file (flymake-init-create-temp-buffer-copy 'flymake-create-temp-inplace))
+           (local-file (file-relative-name temp-file (file-name-directory buffer-file-name)))
+           )
+      (list (concat KUSO_PYTHON_PATH "pyemacs.sh")  (list local-file)))
+    )
+
+  (add-to-list 'flymake-allowed-file-name-masks '("\\.py$" flymake-pyflakes-init))
+  )
---
+++ pyemacs.sh
@@ -0,0 +1,6 @@
+#! /bin/bash
+
+#pylint "$1" 2>/dev/null
+pyflakes "$1"
+pep8 --ignore=E221,E701,E202,E128 --repeat "$1"
+true
