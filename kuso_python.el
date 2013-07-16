8f02ff884c509238f89bdb90d635046ca3aa375d
Sameer Rahmani <lxsameer@gnu.org>
2013-07-16 17:12:05 +0430

    Jedi configured

--- kuso_python.el
+++ kuso_python.el
@@ -1,1 +1,9 @@
+;; Set uncomment keybinding
 (define-key py-mode-map (kbd "\C-c @") 'uncomment-region)
+
+
+;; Setup Jedi
+(autoload 'jedi:setup "jedi" nil t)
+(add-hook 'python-mode-hook 'jedi:setup)
+(setq jedi:setup-keys t)
+(setq jedi:complete-on-dot t)
